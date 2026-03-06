826b0a7413dd5e3e94cafee8965a34d01f519d3e
Kayleen Avendano Reguera <kavendano@ucol.mx>
2026-03-06 11:15:34 -0600

    Diseño de seleccion de acceso

---
+++ seleccion.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Cineblog - Acceso</title>
+    <link rel="stylesheet" href="css/est.css">
+</head>
+<body>
+    <div class="container">
+        <!--Logo y el titulo (header de este apartado) -->
+        <header>
+            <img src="css/cineBlog_Logo.png" alt="Logo" class="logo">
+            <h1>Cineblog</h1>
+        </header>
+
+        <h2>Seleccione cómo desea entrar</h2>
+        
+        <!--Botones de acceso (usuario y visitante) -->
+        <div class="options">
+            <div class="container1">
+                <div class="icon">👤</div>
+                <h3>Visitante</h3>
+                <a href="registro.php" class="btn-option">
+                <p>Acceso limitado sin registro</p>
+                </a>
+            </div>
+            <div class="container1">
+                <div class="icon">👥</div>
+                <h3>Usuario</h3>
+                <a href="inicioSesion.php" class="btn-option">
+                    <p>Acceso completo con registro</p>
+                </a>
+            </div>
+        </div>
+
+        <!-- Link de inicio de sesion -->
+         <footer>
+            <p>¿Ya tienes una cuenta? <a href="inicioSesion.php">Inicia sesión aquí</a></p>
+        </footer>
+    </div>
+</body>
+</html>
